4be5cdbcedc670e253c39c3c1d7931b4b57b4ba6
Edmer Valencia <edmer_franciz@yahoo.com>
2022-04-12 18:12:36 -0500

    added simple code questions

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+//Create 5 easy coding questions
+const questions = [
+    {
+        title:"What does HTML stand for?",
+        choices:["Hypertext Markup Language", "Hypertext Message Language", "Hypertension Mockup Language", "None of the Above" ],
+        answer : "Hypertext Markup Language"
+    },
+    {
+        title:"What does CSS stand for?",
+        choices:["Cascading Show Style","Cascading Style Sheet","Control Source Styling","None of the above"],
+        answer:"Cascading Style Sheet",
+    },
+    {
+        title:"What is used to enclose an array in Javascript?",
+        choices:["{} Curly Brackets","() Parantheses","[] Brackets","All of the above"],
+        answer:"[] Brackets"
+    },
+    {
+        title:"What is the HTML tag that can link other files or sites",
+        choices:["<a>","<h1>","<p>","<href>"],
+        answer:"<href>"
+    },
+    {
+        title:"What git command allows us to make other branches?",
+        choices:["git commit -b", "git checkout -b", "git add -b", "None of the above"],
+        answer:"git checkout -b"
+    },
+];
+
+//Setting variables to add the html elements
